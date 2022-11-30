6ba7481a0f1f6fac245fd4425d111b644ffb12bc
yavuz kaymak <yavuz.kaymak@otto.de>
2022-11-30 19:05:37 +0100

    comparing to influxdb

--- send_metric.sh
+++ send_metric.sh
@@ -6,6 +6,18 @@ read -r -d '' metric << EOM
 rows_insterted{stage="biw", instance="talend", table_name="prd_customer"} 10 
 EOM
 
-
+echo sending metric to Pushgateway
 echo $metric
 echo "$metric" | curl --data-binary @- http://localhost:9091/metrics/job/customer -ks
+
+
+
+
+
+read -r -d '' metric << EOM
+rows_insterted,stage=biw,instance=talend,table_name=prd_customer value=10
+EOM
+
+echo sending metric to Influxdb
+echo $metric
+echo "$metric" | curl -i -XPOST --data-binary @- http://localhost:8086/write?db=mammut -s
