ad93fdba8a8a3472771601ed60ef77c69555ac1b
yavuz kaymak <yavuz.kaymak@otto.de>
2022-12-13 13:48:35 +0100

    (doc) comments

--- send_metric.sh
+++ send_metric.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-
+## send metric to pushgateway
 read -r -d '' metric << EOM
 # TYPE rows_insterted counter
 rows_insterted{stage="biw", instance="talend", table_name="prd_customer"} 10 
@@ -11,7 +11,7 @@ echo $metric
 echo "$metric" | curl --data-binary @- http://localhost:9091/metrics/job/customer -ks
 
 
-
+## send metric to influxdb
 read -r -d '' metric << EOM
 rows_insterted,stage=biw,instance=talend,table_name=prd_customer value=10
 EOM
@@ -20,7 +20,7 @@ echo sending metric to Influxdb
 echo $metric
 echo "$metric" | curl -i -XPOST --data-binary @- http://localhost:8086/write?db=mammut -s
 
-
+## send metric to graphite
 read -r -d '' metric << EOM
 rows_insterted;stage=biw;instance=talend;table_name=prd_customer 10 $(date +%s)
 EOM
